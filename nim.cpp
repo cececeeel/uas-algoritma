d85e03a036f539b0af1c23d3e8ce75629c307da8
cececeeel <cesy0017@gmail.com>
2026-01-06 23:02:28 +0700

    codingan nim baru

--- nim.cpp
+++ nim.cpp
@@ -1,1 +1,38 @@
-uas nim 
+#include <iostream>
+using namespace std;
+
+    int main() {
+        string nama[10];
+        int berat[10];
+        int totalharga[10];
+        int jumlah;
+        int HargaPerKg = 10000;
+
+        cout << "=== APLIKASI PENGELOLAAN LAUNDRY ===" << endl;
+        cout << "Masukan Jumlah Pelangga: ";
+        cin >> jumlah; 
+
+        for (int i = 0; i < jumlah; i++) {
+            cout << "\nPelanggan ke-" << i + 1 << endl;
+            cout << " Nama         :";
+            cin >> nama[i];
+            cout << " Berat Laundry:";
+            cin >> berat[i];
+
+            if (berat[i] > 0) {
+                totalharga[i] = berat[i] * HargaPerKg;
+            } else {
+                totalharga[i] = 0;
+            }
+        }
+        cout << "\n=== DAFTAR LAUNDRY ===" << endl;
+        cout << "nama\tberat\ttoal Harga" << endl;
+
+        for (int i = 0; i < jumlah; i++) {
+            cout << nama[i] << "\t"
+                 << berat[i] << "Kg\tRp"
+                 << totalharga[i] << endl;
+        }
+        cout << "\n TERIMA KASIH" << endl;
+        return 0;
+    }
